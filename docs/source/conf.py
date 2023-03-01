3a24233e1f284dfe777b2ba136ac66de705ba8b9
bradley-smith-gpa <bradley.smith@gpa.gov.uk>
2023-03-01 15:25:10 +0000

    Ignore only docs subdir

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,10 +9,13 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+source_code_relative_path = '../..'
+source_code_abs_path = os.path.abspath(source_code_relative_path)
+print(source_code_abs_path)
+sys.path.insert(0, source_code_abs_path)
 
 
 # -- Project information -----------------------------------------------------
@@ -28,8 +31,10 @@ author = 'Bradley Smith'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
 ]
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -34,3 +34,4 @@ Contents
 .. toctree::
 
    usage
+   api
