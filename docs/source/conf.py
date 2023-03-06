1e85c54eb9abcad3d5ead5c15c680c5f1eea0280
bradley-smith-gpa <bradley.smith@gpa.gov.uk>
2023-03-06 09:51:48 +0000

    Add favicon to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,3 +54,6 @@ html_theme = 'press'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+favicon_path = source_code_abs_path / 'graphics/favicon.png'
+html_favicon = f'{favicon_path}'
